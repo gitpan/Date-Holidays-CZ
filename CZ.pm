5e25a32df2727194d22c3277a0dff0854f529b33
Nathan Cutler <presnypreklad@gmail.com>
2013-12-27 11:07:38 +0000

    Import of SMITHFARM/Date-Holidays-CZ-0.04 from CPAN.
    gitpan-cpan-distribution: Date-Holidays-CZ
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SMITHFARM/Date-Holidays-CZ-0.04.tar.gz
    gitpan-cpan-author:       SMITHFARM
    gitpan-cpan-maturity:     released

--- CZ.pm
+++ CZ.pm
@@ -12,11 +12,11 @@ Date::Holidays::CZ - determine Czech Republic bank holidays
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
